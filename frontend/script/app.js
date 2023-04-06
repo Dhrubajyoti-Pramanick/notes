8c81bddaea97159cc707c44abee1a30fdb9be1ed
Dhrubajyoti-Pramanick <dhrubajyoti@itobuz.com>
2023-04-06 19:32:38 +0530

    2nd commit

--- frontend/script/app.js
+++ frontend/script/app.js
@@ -1,8 +1,3 @@
-function color() {
-  let colors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info'];
-  let random_color = colors[(Math.floor(Math.random() * colors.length))];
-  return random_color;
-}
 async function getAllNotes() {
   const response = await fetch("http://localhost:4000/notes/get", {
     method: "GET",
@@ -13,15 +8,15 @@ async function getAllNotes() {
   console.log(response);
   console.log(response.data.length);
   for (let i = 0; i < response.data.length; i++) {
-    let bg = color();
     document.getElementById(
       "log"
-    ).innerHTML += `<div id="logs" class="col-4 card card-body bg-${bg} text-white rounded-3 p-3 my-1">
+    ).innerHTML += `<div id="logs" class="card card-body text-white rounded-3 p-3 my-1">
     <div class="d-flex justify-content-between">
       <div id="subTitle" class="card-title fs-3">${response.data[i].title}</div>
       <div>
-        <button id="del" class="buttonSvg btn btn-info p-0"><img class="w-50" src="./images/bin.svg" alt="bin"></button>
-      <button id="edit" class="buttonSvg btn btn-info p-0"><img class="w-50 " src="./images/edit.svg" alt="edit"></button>
+        <button id="del" class="buttonSvg btn p-0"><i class="fa-solid fa-trash"></i></button>
+      <button id="edit" class="buttonSvg btn p-0"><i class="fa-solid fa-pen"></i></button>
+      <button id="update" class="buttonSvg btn p-0" onclick="visibility2()"><i class="fa-solid fa-repeat"></i></button>
       </div>
       </div>
     <div id="subContent" class="card-text">${response.data[i].content}</div>
@@ -30,10 +25,10 @@ async function getAllNotes() {
 </section>`;
     del();
     edit();
+    update();
   }
 }
 getAllNotes();
-
 
 //creating notes
 function create() {
@@ -44,16 +39,16 @@ function create() {
     alert("Fill both the inputs");
   } else {
     // writing log of notes
-    let bg = color();
     document.getElementById("message").innerHTML = "*Successfully Added";
     document.getElementById(
       "log"
-    ).innerHTML += `<div id="logs" class="card card-body bg-${bg} text-white rounded-3 p-3 my-1">
+    ).innerHTML += `<div id="logs" class="card card-body text-white rounded-3 p-3 my-1">
           <div class="d-flex justify-content-between">
             <div id="subTitle" class="card-title fs-3">${title}</div>
             <div>
-              <button id="del" class="buttonSvg btn btn-info p-0"><img class="w-50" src="./images/bin.svg" alt="bin"></button>
-              <button id="edit" class="buttonSvg btn btn-info p-0"><img class="w-50" src="./images/edit.svg" alt="edit"></button>
+              <button id="del" class="buttonSvg btn btn-info p-0"><i class="fa-solid fa-trash"></i></button>
+              <button id="edit" class="buttonSvg btn btn-info p-0"><i class="fa-solid fa-pen"></i></button>
+              <button id="update" class="buttonSvg btn btn-info p-0" onclick="visibility2()"><i class="fa-solid fa-repeat"></i></button>
             </div>
           </div>
           <div id="subContent" class="card-text">${content}</div>
@@ -81,6 +76,7 @@ function create() {
   }
   del();
   edit();
+  update();
 }
 //deleting notes
 function del() {
@@ -91,6 +87,10 @@ function del() {
       let id = parent.lastElementChild.innerHTML;
       console.log(id);
       parent.remove();
+      document.getElementById("message").innerHTML = "*Successfully Deleted";
+      setTimeout(() => {
+        document.getElementById("message").innerHTML = null;
+      }, 2000);
       fetch("http://localhost:4000/notes/delete", {
         method: "DELETE",
         headers: {
@@ -104,16 +104,28 @@ function del() {
   }
 }
 // updating notes
+function update() {
+  // arr = document.querySelector("#update");
+  // console.log(arr);
+  for (i = 0; i < arr.length; i++) {
+    arr[i].onclick = () => {
+      console.log("hello", i, arr[i]);
+    };
+  }
+}
 function edit() {
   arr = document.querySelectorAll("#edit");
+  // console.log(arr.length);
   for (i = 0; i < arr.length; i++) {
     arr[i].onclick = function () {
       subTitle = this.parentNode.parentNode.firstElementChild;
       console.log(subTitle.innerHTML);
       subContent = this.parentNode.parentNode.parentNode.children[1];
       console.log(subContent.innerHTML);
-      id = this.parentNode.parentNode.parentNode.lastElementChild.innerHTML
-      console.log(this.parentNode.parentNode.parentNode.lastElementChild.innerHTML)
+      id = this.parentNode.parentNode.parentNode.lastElementChild.innerHTML;
+      console.log(
+        this.parentNode.parentNode.parentNode.lastElementChild.innerHTML
+      );
       let title = document.getElementById("title").value;
       let content = document.getElementById("content").value;
       if (title === "" || content === "") {
@@ -121,6 +133,10 @@ function edit() {
       } else {
         subTitle.innerHTML = title;
         subContent.innerHTML = content;
+        document.getElementById("message").innerHTML = "*Successfully Edited";
+        setTimeout(() => {
+          document.getElementById("message").innerHTML = null;
+        }, 2000);
         fetch("http://localhost:4000/notes/update", {
           method: "PUT",
           headers: {
@@ -143,5 +159,60 @@ function edit() {
           });
       }
     };
+  }
+}
+function edit2() {
+  arr = document.querySelectorAll("#edit");
+  console.log(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    // arr[i].onclick() = function () {
+    //   console.log(arr[i]);
+    // }
+  }
+}
+// for(i=0;i<arr.length;i++){
+//   arr[i].onclick() = function () {
+//     console.log(this.parentNode);
+//   }
+// }
+let flag = true;
+function visibility() {
+  if (flag) {
+    document.getElementById("form").innerHTML = `
+    <div id="section2" class="col-10 col-md-3 d-flex flex-column p-3 gap-3">
+      <input type="text" id="title" placeholder="Enter Title">
+      <textarea id="content" placeholder="Enter Content"></textarea>
+      <button id="add" class="btn text-white" onclick="create()">Add</button>
+    </div>`;
+    document.getElementById(
+      "cross"
+    ).innerHTML = `<i class="fa-solid fa-xmark" style="color: #ffffff;" onclick="visibility()"></i>`;
+    flag = false;
+  } else {
+    document.getElementById("form").innerHTML = ``;
+    document.getElementById(
+      "cross"
+    ).innerHTML = `<i class="fa-solid fa-plus" style="color:white" onclick="visibility()"></i>`;
+    flag = true;
+  }
+}
+function visibility2() {
+  if (flag) {
+    arr = document.querySelectorAll("#edit");
+  console.log(arr.length);
+  console.log(arr[3].parentNode.parentNode.parentNode.firstElementChild.firstElementChild.innerHTML);
+  for (let i = 0; i < arr.length; i++) {
+  }
+    document.getElementById("form").innerHTML = `
+    <div id="section2" class="col-10 col-md-3 d-flex flex-column p-3 gap-3">
+      <input type="text" id="title" placeholder="Enter Title">
+      <textarea id="content" placeholder="Enter Content"></textarea>
+      <button id="add" class="btn text-white" onclick="edit2()">Edit</button>
+    </div>`;
+    console.log();
+    flag = false;
+  } else {
+    document.getElementById("form").innerHTML = ``;
+    flag = true;
   }
 }
